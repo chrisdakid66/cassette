175ed67a9e8baaac8fca94d4e55119117c33c615
chrisdakid66 <christopher.rice.1997@gmail.com>
2026-09-20 20:55:18 -0600

    chore(settings): identify Nook v0.9 builds

--- Cassette/Views/Settings/SettingsView.swift
+++ Cassette/Views/Settings/SettingsView.swift
@@ -87,10 +87,10 @@ struct SettingsView: View {
             }
 
             Section {
-                LabeledContent("Chrasssette Build", value: "Nook v0.8")
+                LabeledContent("Chrasssette Build", value: "Nook v0.9")
                 LabeledContent("App Version", value: appVersion)
                 LabeledContent("GitHub Run", value: "#\(buildNumber)")
-                LabeledContent("Branch", value: "chrasssette-nook-v0.8")
+                LabeledContent("Branch", value: "chrasssette-nook-v0.9")
             } header: {
                 Text("Build")
             } footer: {
